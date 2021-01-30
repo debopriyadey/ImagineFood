38d99ff4fd11341e17263c86791e387c7bd7ee09
debopriyadey <debodey158@gmail.com>
2021-01-31 01:45:23 +0530

    3rd commit menu left

--- resources/script/app.js
+++ resources/script/app.js
@@ -1,6 +1,6 @@
 //1 price
 window.addEventListener('scroll',function(){
-    var imag=document.querySelector('.plans .plan-box:nth-child(1)');
+    var imag=document.querySelector('.plans .plan-box-one');
     var imgposition=imag.getBoundingClientRect().top;
     var imgpositio=imag.getBoundingClientRect().top+imag.getBoundingClientRect().height;
     var screem=window.innerHeight/1.5;
@@ -17,32 +17,10 @@ window.addEventListener('scroll',function(){
 }
 
 )
-// 2 app last
-window.addEventListener('scroll',function(){
-    var imag=document.querySelector('.plans .plan-box:nth-child(2)');
-    var imag=document.querySelector('.plans .plan-box:nth-child(2)');
-    var imgposition=imag.getBoundingClientRect().top;
-    var imgpositio=imag.getBoundingClientRect().top+imag.getBoundingClientRect().height;
-    var screem=window.innerHeight/1.5;
-    var scree=window.innerHeight/8;
-  if(imgpositio<scree){ 
-        imag.style.transform='translateY(50px)';
-        imag.style.opacity='0';
-         }
-   else  if(imgposition<screem){ 
-    imag.style.transform='translateY(0)';
-    imag.style.opacity='1';
-     }
-   else  if(imgposition>screem){ 
-    imag.style.transform='translateY(50px)';
-    imag.style.opacity='0';
-     }
-}
 
-)
 // omni app last
 window.addEventListener('scroll',function(){
-    var imag=document.querySelector('.plans .plan-box:nth-child(3)');
+    var imag=document.querySelector('.plans .plan-box-two');
     var imgposition=imag.getBoundingClientRect().top;
     var imgpositio=imag.getBoundingClientRect().top+imag.getBoundingClientRect().height;
     var screem=window.innerHeight/1.5;
@@ -55,25 +33,6 @@ window.addEventListener('scroll',function(){
      }
    else  if(imgposition>screem){ 
     imag.style.transform='translateX(200vw)';
-     }
-}
-
-)
-// ANIMATION
-window.addEventListener('scroll',function(){
-    var imag=document.querySelector('.section-feature .box-parent');
-    var imgposition=imag.getBoundingClientRect().top;
-    var imgpositio=imag.getBoundingClientRect().top+imag.getBoundingClientRect().height;
-    var screem=window.innerHeight/1.5;
-    var scree=window.innerHeight/8;
-  if(imgpositio<scree){ 
-        imag.style.opacity=0;
-         }
-   else  if(imgposition<screem){ 
-    imag.style.opacity=1;
-     }
-   else  if(imgposition>screem){ 
-    imag.style.opacity=0;
      }
 }
 
